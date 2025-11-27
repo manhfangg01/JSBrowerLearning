6de193b00cc9532b35c4eac51f04d943e8820f03
PhanVanManh <imseirousmanh@gmail.com>
2025-11-27 20:11:16 +0700

    lab 17

---
+++ lab17/lab17.js
@@ -0,0 +1,13 @@
+console.log("hello lab17");
+
+var name = "manhpha";
+
+const name1 = "manhchan";
+name1 = 109;
+
+let nam = "manh";
+
+console.log(name1);
+
+// let có thể kiểm soát biến trong scope: hết scope -> hủy  => nên sử dụng
+// var chỉ chạy được thôi: hết scope -> vẫn xài được
