04e0aefbf8259f36b294d04d4d71b32c5d500c58
PhanVanManh <imseirousmanh@gmail.com>
2025-11-27 20:46:29 +0700

    lab 22

---
+++ lab22/lab22.js
@@ -0,0 +1,8 @@
+const name = "Manh";
+const age = 1999;
+
+const intro = `Hello my name is ${name},
+currentl I am ${age} years-old `;
+// Chèn trực tiếp biến và biểu thức không cần cộng chuỗi
+// Không cần /n để xuống dòng
+console.log(intro);
